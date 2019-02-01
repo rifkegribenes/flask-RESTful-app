f871b1999254e2d36279a4f011620881f857ff5d
rifkegribenes <rifkegribenes@gmail.com>
2019-01-31 21:41:22 -0800

    auth pt 2

--- code/app.py
+++ code/app.py
@@ -1,16 +1,22 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'jsfei;mwjjt8y7gwe0jc8y*&)JIkjfbjdh'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)
+
 items = []
 
 class Item(Resource):
+	@jwt_required()
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
-		return {'item': None }, 200 if item else 404
+		return {'item': item }, 200 if item else 404
 
 	def post(self, name):
 		if next(filter(lambda x: x['name'] == name, items), None) is not None:

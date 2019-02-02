366e29ad70ae977513c4ff6e5e803fe185d2c529
rifkegribenes <rifkegribenes@gmail.com>
2019-02-01 22:25:57 -0800

    refactor and add reqparse

--- code/app.py
+++ code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,13 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+		type=float,
+		reqired=True,
+		help="This field cannot be blankß"
+		)
+
 	@jwt_required()
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
@@ -22,7 +29,7 @@ class Item(Resource):
 		if next(filter(lambda x: x['name'] == name, items), None) is not None:
 			return {'message': 'An item named {} already exists.'.format(name)}, 400
 
-		data = request.get_json()
+		data = Item.parser.parse_args()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
@@ -33,7 +40,8 @@ class Item(Resource):
 		return {'message': 'Item deleted.'}
 
 	def put(self, name):
-		data = request.get_json()
+		data = Item.parser.parse_args()
+
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		if item is None:
 			item = {'name': name, 'price': data['price']}
